f42e37d89c1b50f895bf54eecc5ad269a4e11ab4
Jonas Larsson <jonas@hallerud.se>
2010-12-22 10:52:13 -0500

    Build libgstqtmux from gst-plugins-bad-0.10.20

--- jni/gst-plugins-bad-0.10.20/gst/Android.mk
+++ jni/gst-plugins-bad-0.10.20/gst/Android.mk
@@ -7,3 +7,4 @@ GST_PBAD_GST := $(LOCAL_PATH)
 include $(GST_PBAD_GST)/audioparsers/Android.mk
 include $(GST_PBAD_GST)/h264parse/Android.mk
 include $(GST_PBAD_GST)/mpegdemux/Android.mk
+include $(GST_PBAD_GST)/qtmux/Android.mk
