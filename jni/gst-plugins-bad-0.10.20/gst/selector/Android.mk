a035ff487efbbff2e642aa046f4df02faee80f1f
Jonas Larsson <jonas@hallerud.se>
2011-01-05 01:44:58 +0100

    Fix gstselector plugin

--- jni/gst-plugins-bad-0.10.20/gst/selector/Android.mk
+++ jni/gst-plugins-bad-0.10.20/gst/selector/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES :=      \
+  gstselector.c         \
   gstinputselector.c    \
   gstoutputselector.c   \
   gstselector-marshal.c
